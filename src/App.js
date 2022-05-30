7b2af091602dd7096dcdaaff187f61b64a78973d
byakko1022 <ravenclaw946@gmail.com>
2022-05-30 05:49:49 -0500

    readme

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import './index.css';
 
 export default function App() {
   return (
